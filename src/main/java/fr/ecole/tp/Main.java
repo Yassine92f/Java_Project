b34ce2cef71fd83b16a3a956248290ec1e334d6a
SmainBelghazi <smainbelghazi@hotmail.com>
2026-05-29 12:50:32 +0200

    Résolution conflit Main.java + mise à jour ProduitService

--- src/main/java/fr/ecole/tp/Main.java
+++ src/main/java/fr/ecole/tp/Main.java
@@ -4,9 +4,22 @@ import fr.ecole.tp.service.ProduitService;
 import fr.ecole.tp.util.DataFactory;
 import fr.ecole.tp.model.Produit;
 
+
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Produit p = DataFactory.getProduits().get(0);
+        Produit p = DataFactory.getProduits().get(2);
         System.out.println(ProduitService.nomVariable.apply(p));
+        ProduitService.afficherTousLesProduits(DataFactory.getProduits());
+         
+        
+
+        
+        
+        
+
     }
 }
--- src/main/java/fr/ecole/tp/service/ProduitService.java
+++ src/main/java/fr/ecole/tp/service/ProduitService.java
@@ -4,9 +4,22 @@ package fr.ecole.tp.service;
 import java.util.function.Function;
 
 import fr.ecole.tp.model.Produit;
+import java.util.List;
+import java.util.function.Consumer;
+
 
 
 public class ProduitService {
 
     public static Function<Produit, String> nomVariable =  produit -> "[" + produit.getCategorie().toUpperCase() + "]"+ " " + produit.getNom() + " - " + produit.getPrix() + " €";
+
+
+
+    public static void afficherTousLesProduits(List<Produit> produits) {
+        Consumer<Produit> afficherProduit = System.out::println;
+        produits.forEach(afficherProduit);
+    }
 }
+
+
+
