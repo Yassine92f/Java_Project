a1cbbe7c494bd957054844bf6207c62d952c89b4
Yass <yass@MacBook-Air-5.local>
2026-05-29 16:09:01 +0200

    Ajout de la console (menu interactif)

--- src/main/java/fr/ecole/tp/Main.java
+++ src/main/java/fr/ecole/tp/Main.java
@@ -4,62 +4,79 @@ import fr.ecole.tp.service.ProduitService;
 import fr.ecole.tp.service.CommandeService;
 import fr.ecole.tp.util.DataFactory;
 import fr.ecole.tp.model.Produit;
-import fr.ecole.tp.model.Commande;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Produit> produits = DataFactory.getProduits();
+        public static void main(String[] args) {
+                List<Produit> produits = DataFactory.getProduits();
+                Scanner scanner = new Scanner(System.in);
+                int choix = -1;
 
-        // Fonctionnalité 1
-        ProduitService.afficherTousLesProduits(produits);
+                while (choix != 0) {
+                        System.out.println("\n=== CATALOGUE CONSOLE ===\n");
+                        System.out.println("1. Afficher tous les produits");
+                        System.out.println("2. Afficher les produits triés par prix croissant");
+                        System.out.println("3. Afficher les produits d'une catégorie");
+                        System.out.println("4. Rechercher les produits en promotion");
+                        System.out.println("5. Afficher les noms des produits en majuscules");
+                        System.out.println("6. Afficher les 3 produits les plus chers");
+                        System.out.println("7. Calculer le prix total du catalogue");
+                        System.out.println("8. Afficher les catégories uniques");
+                        System.out.println("9. Afficher les commandes avec leurs produits");
+                        System.out.println("10. Calculer le total de chaque commande");
+                        System.out.println("11. Vérifier si tous les produits ont un prix positif");
+                        System.out.println("12. Démonstration parallelStream");
+                        System.out.println("0. Quitter");
+                        System.out.print("\nVotre choix : ");
 
-        // Fonctionnalité 2
-        Produit p = DataFactory.getProduits().get(0);
-        System.out.println(ProduitService.nomVariable.apply(p));
+                        choix = scanner.nextInt();
 
-        // Fonctionnalité 3
-        ProduitService.Filtrage(produits);
-
-        // Fonctionnalité 4
-        ProduitService.FiltrageMultiple(produits);
-
-        // Fonctionnalité 5
-        ProduitService.TransformationMap(produits);
-
-        // Fonctionnalité 6
-        ProduitService.TriAvecComparator(produits);
-
-        // Fonctionnalité 7
-        ProduitService.AvecDistinct(produits);
-        ProduitService.AvecSet(produits);
-
-        // Fonctionnalité 8
-        ProduitService.AfficherLimit(produits);
-
-        // Fonctionnalité 9
-        ProduitService.Pagination(produits);
-
-        // Fonctionnalité 10
-        ProduitService.PrixTotal(produits);
-
-        // Fonctionnalité 11
-        ProduitService.PrixTotalAvecMap(produits);
-
-        // Fonctionnalité 12
-        ProduitService.Traitement(produits);
-
-        // Fonctionnalité 13
-        CommandeService.afficherProduitsCommandes(DataFactory.getCommandes());
-
-
-        // Fonctionnalité 14
-        CommandeService.totalToutesCommande(DataFactory.getCommandes());
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-
-        // Fonctionnalité 15
-        CommandeService.mapString(DataFactory.getCommandes());
-
-    }
+                        switch (choix) {
+                                case 1:
+                                        ProduitService.afficherTousLesProduits(produits);
+                                        break;
+                                case 2:
+                                        ProduitService.TriAvecComparator(produits);
+                                        break;
+                                case 3:
+                                        ProduitService.Filtrage(produits);
+                                        break;
+                                case 4:
+                                        ProduitService.Filtrage(produits);
+                                        break;
+                                case 5:
+                                        ProduitService.TransformationMap(produits);
+                                        break;
+                                case 6:
+                                        ProduitService.AfficherLimit(produits);
+                                        break;
+                                case 7:
+                                        ProduitService.PrixTotal(produits);
+                                        break;
+                                case 8:
+                                        ProduitService.AvecDistinct(produits);
+                                        break;
+                                case 9:
+                                        CommandeService.afficherProduitsCommandes(DataFactory.getCommandes());
+                                        break;
+                                case 10:
+                                        CommandeService.totalToutesCommande(DataFactory.getCommandes());
+                                        break;
+                                case 11:
+                                        ProduitService.Traitement(produits);
+                                        break;
+                                case 12:
+                                        System.out.println("Démonstration parallelStream à implémenter");
+                                        break;
+                                case 0:
+                                        System.out.println("Au revoir !");
+                                        break;
+                                default:
+                                        System.out.println("Choix invalide");
+                        }
+                }
+                scanner.close();
+        }
 }
