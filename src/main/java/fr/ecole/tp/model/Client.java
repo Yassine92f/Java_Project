4a79bc9f4343a1935e4915bea94f037d32e2dec4
Yass <yass@MacBook-Air-5.local>
2026-05-29 10:08:34 +0200

    Création des différentes classes

---
+++ src/main/java/fr/ecole/tp/model/Client.java
@@ -0,0 +1,16 @@
+package fr.ecole.tp.model;
+
+
+public Class Client {
+
+    private final String id;
+    private final String nom;
+    private final String email;
+
+
+    public Client(String id, String nom, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.email = email;
+    }
+}
